4c30b85d796bf501288c2e6c0612c746d7c2f216
Bryan Hunt <bhunt@mac.com>
2015-02-14 12:16:57 -0600

    Add split view

---
+++ app/routes/split.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+//import TreeNode from 'ember-idx-tree/node';
+
+export default Ember.Route.extend({
+/*  model: function() {
+    var family, gaya, josh, lud, moses, suz, verdi;
+    family = TreeNode.create({
+      title: 'Family'
+    });
+    suz = family.createChild({
+      title: 'Susan'
+    });
+    lud = family.createChild({
+      title: 'Luda'
+    });
+    josh = suz.createChild({
+      title: 'Josh'
+    });
+    moses = suz.createChild({
+      title: 'Moses'
+    });
+    verdi = lud.createChild({
+      title: 'Verdi'
+    });
+    gaya = lud.createChild({
+      title: 'Gaya'
+    });
+
+    return family;
+  }*/
+});
